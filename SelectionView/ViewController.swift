7b1a0debdc0c064ce6dd7bb70708afd247ccf81e
Apple <praveen@apples-macbook-pro.local>
2019-03-25 15:05:41 +0530

    Initial Commit

---
+++ SelectionView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SelectionView
+//
+//  Created by praveen on 3/25/19.
+//  Copyright © 2019 focussoftnet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
