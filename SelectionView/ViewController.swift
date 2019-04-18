d4a413e449a7c831ec716f7309f9033ee65110af
Apple <praveen@apples-macbook-pro.local>
2019-04-18 16:06:52 +0530

    VC

--- SelectionView/ViewController.swift
+++ SelectionView/ViewController.swift
@@ -13,6 +13,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let selectionView = CLSelectionView()
+        selectionView.translatesAutoresizingMaskIntoConstraints = false
+        selectionView.backgroundColor = .white
+        view.addSubview(selectionView)
+        
+        selectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        selectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        selectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+        selectionView.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        
     }
 
     override func didReceiveMemoryWarning() {
